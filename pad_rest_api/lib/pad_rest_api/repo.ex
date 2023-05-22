a57c2564f9380a2cdda2140eca37c3531e4a7863
david doria <dor15004@byui.edu>
2023-05-22 14:17:48 -0600

    Switched from Elixir backend to flask backend

--- pad_rest_api/lib/pad_rest_api/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule PadRestApi.Repo do
-  use Ecto.Repo,
-    otp_app: :pad_rest_api,
-    adapter: Ecto.Adapters.Postgres
-end
--- pad_rest_api/lib/pad_rest_api_web/controllers/default_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule PadRestApi.DefaultController do
-  use PadRestApiWeb, :controller
-
-  def index(conn, _params) do
-    text conn, "Pick A Dish API! - #{Mix.env()}"
-  end
-end
